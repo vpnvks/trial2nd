88879f3f44faf5b818754d8dc10321dfa52453fc
VipinMishra <VipinMishra@DESKTOP-ERBTEJN>
2019-11-20 09:54:11 +0530

    hi

---
+++ src/test/java/maven_jenk/maven_jenk/Print_jenk.java
@@ -0,0 +1,11 @@
+package maven_jenk.maven_jenk;
+
+import org.testng.annotations.Test;
+
+public class Print_jenk {
+
+	@Test
+	public void print1() {
+		System.out.println("jenkins test passed");
+	}
+}
